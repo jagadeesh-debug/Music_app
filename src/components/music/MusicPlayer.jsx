3beb730418f2dd6ff6e34034dd382d584ef580ea
Chandrashekhar Kachawa <42296139+mcctrix@users.noreply.github.com>
2025-01-02 16:57:49 +0530

    fixed mute icon issue

--- src/components/music/MusicPlayer.jsx
+++ src/components/music/MusicPlayer.jsx
@@ -17,7 +17,7 @@ import { Button } from "../ui/button";
 import { useStore, useFetch } from "../../zustand/store";
 
 function MusicPlayer() {
-  const [volume, setVolume] = useState(localStorage.getItem("volume") || 0.5);
+  const [volume, setVolume] = useState(localStorage.getItem("volume") === null ? 0.5 : localStorage.getItem("volume"));
   const [muted, setMuted] = useState(false);
   const [played, setPlayed] = useState(0);
   const [duration, setDuration] = useState(0);
@@ -54,7 +54,8 @@ function MusicPlayer() {
 
   const handlePlayPause = () => setIsPlaying(!isPlaying);
   const handleVolumeChange = (e) => {
-    localStorage.setItem("volume", e.target.value);
+    localStorage.setItem("volume", Number(e.target.value));
+    console.log(localStorage.getItem("volume"));
     setVolume(parseFloat(e.target.value));
     setMuted(false);
   };
