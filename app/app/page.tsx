cbc3a65e1ff8ec687f4c4f5f02505829407de2e4
Chandrashekhar Kachawa <42296139+mcctrix@users.noreply.github.com>
2025-01-17 15:36:30 +0530

    fixed zustand code for Music Player

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,22 @@
+"use client"
+
+import goBackendApi from "@/api/gobackend";
+import { useEffect } from "react";
+
+const fetchSuggestions = async ()=> {
+  const res = await goBackendApi.get("/homesuggestion?query=trend")
+  return res.data
+}
+
 const Page = () => {
+  useEffect(()=>{
+    console.log(fetchSuggestions())
+  },[])
   return <div>Hello World</div>;
 };
+
+const Card = ()=>{
+  return <div>Card</div>
+}
 
 export default Page;
---
+++ zustand/golang.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+// import Api from "@/api/jiosavan";
+
+interface MusicPlayerState {
+  musicID: string;
+  setMusicID: (id: string) => void;
+  isPlaying: boolean;
+  setIsPlaying: (isPlaying: boolean) => void;
+  queue: song[];
+  addToQueue: (song: song) => void;
+}
+
+interface song {
+    Name: string;
+    Id: string;
+    Images: SpotifyImages[];
+    // Views: number;
+}
+
+interface SpotifyImages {
+    Height: number;
+    Width: number;
+    URL: string;
+}
+
+export const useMusicPlayerStore = create<MusicPlayerState>()((set) => ({
+    musicID: "",
+    setMusicID: (id) => set({ musicID: id }),
+    isPlaying: false,
+    setIsPlaying: (isPlaying) => set({ isPlaying }),
+    queue: [],
+    addToQueue: (song) => set((state) => ({ queue: [...state.queue, song] })),
+}));
+
+// interface FetchState {
+//     fetchSong: (id: string) => void;
+// }
+  
+// export const useFetchStore = create<FetchState>()((set) => ({
+//    fetchSong: async (id) => {
+//     const song = await Api.get("")
+//    }
+// }))
